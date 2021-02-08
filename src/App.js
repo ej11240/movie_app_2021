dadc983124667a548ecbdd45d23db95a05e9a010
ej11240 <hwangej1124@gmail.com>
2021-02-08 16:17:30 +0900

    영화 웹사이트 완성

--- src/App.js
+++ src/App.js
@@ -1,66 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
-const foodILike=[
-  {
-    id:1,
-    name: "떡볶이",
-    image:"https://media-cdn.tripadvisor.com/media/photo-s/12/31/92/d9/1519804025288-largejpg.jpg",
-    rating:5
-  },
-  {
-    id:2,
-    name:"라면",
-    image:"https://mblogthumb-phinf.pstatic.net/MjAyMDA1MjZfNDMg/MDAxNTkwNDgxOTc1OTE1.rpezhYUxGHG0X6-dmVwuACnqm7AugH9CUjxEatcVNsAg.C27glH_kXPk5zTORLyjUbU_yjkGDEbmwZXjaq_xGltIg.JPEG.naverschool/%ED%98%B8%EB%A1%9C%EB%A1%9C%EB%A1%9D.jpg?type=w800",
-    rating:3
-  },
-  {
-    id:3,
-    name:"에그마요 salad",
-    image:"https://mblogthumb-phinf.pstatic.net/MjAyMDEyMDhfMTI4/MDAxNjA3MzU1NjcyOTg3._Ql3p8iDGizLQ3EYAcsM8sT2ElW-b9gnN68M-AOP6X4g.2PZ0tYBuJH5UdIvEJ0MZSmitiwsuQBeV47ojgCTRoI4g.JPEG.guswl3974/IMG_1190.jpg?type=w800",
-    rating:5
-  },
-  {
-    id:4,
-    name:"들깨칼국수",
-    image:"https://t1.daumcdn.net/cfile/tistory/998364365A84CAEB0A",
-    rating:5
-  },
-  {
-    id:5,
-    name:"보리비빔밥",
-    image:"https://lh3.googleusercontent.com/proxy/5ySu_okgtlfM6f2VxJ9BUwIZ-Ah8nK5a-LGP7aIQTuTYNNBl4IjAYY5m3QCHr8_f3a8uzWuLcUBFwVne5vFW7qy8x0yzzfFC6JycyJS8TZ0",
-    rating:4
+class App extends React.Component {
+  
+  state = {
+    isLoading: true,
+    movies:[]
   }
-]
+  
+  getMovies=async()=>{
+    const {data: {data :{movies}}} =await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({movies,isLoading:false});
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
 
-function Food({ name, image, rating}){
-  return (
-    <div>
-      <h2> I love {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={image} width="200px" alt={name}/>
-      <hr/>
-    </div>
-  )
-}
-Food.propTypes={
-  id: PropTypes.number.isRequired,
-  name:PropTypes.string.isRequired,
-  image:PropTypes.string.isRequired,
-  rating:PropTypes.number.isRequired
-};
-function renderFood(dish){
-  // console.log(dish)
-  return <Food key={dish.id} name={dish.name} image={dish.image} rating={dish.rating}/>
-}
-
-function App() {
-  return (
-    <div align="center">
-      {foodILike.map(renderFood)}
-    </div>
-  );
+  render() {
+    const { isLoading,movies}=this.state;
+    return (
+      <section className="container">
+        { isLoading ? (<div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie =>{
+              console.log(movie);
+              return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres} />
+              })
+            }
+          </div>
+        )}
+      </section>
+      );
+  }
 }
 
 export default App;
